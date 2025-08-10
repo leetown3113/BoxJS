f6b07333cbfa60bb455635bb234403e4e2c83d6e
LeeTown <140289459+leetown3113@users.noreply.github.com>
2025-08-10 13:10:30 +0800

    Update ql_cookie_sync.js

--- JD/ql_cookie_sync.js
+++ JD/ql_cookie_sync.js
@@ -1,6 +1,6 @@
 /*
 青龙 docker 每日自动同步 boxjs cookie
-40 * * * https://raw.githubusercontent.com/dompling/Script/master/jd/ql_cookie_sync.js
+40 * * * https://raw.githubusercontent.com/leetown3113/BoxJS/refs/heads/main/JD/ql_cookie_sync.js
  */
 
 const $ = new API("ql", true);
